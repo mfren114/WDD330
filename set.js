aad73349c5a43de91df2c6c935b18f0527e5160e
mfren114 <mathew.frenette@yahoo.com>
2026-01-28 17:03:00 -0800

    update

---
+++ set.js
@@ -0,0 +1,7 @@
+const startTime = 10
+
+const countDown = document.getElementById("countdown");
+
+const startButton = document
+    .getElementById("startbutton")
+    .addEventListener("click", addToCartHandler);
--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,11 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+[]
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
@@ -19,3 +20,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
